a48282da14119d854cc3e29ab85c2fdf20a60936
Artem Fedoniuk web2025-1 <iiilssofff@gmail.com>
2026-04-26 21:04:37 +0300

    додав InventoryContext для управління станом

--- src/store/InventoryContext.jsx
+++ src/store/InventoryContext.jsx
@@ -1,1 +1,21 @@
-﻿
+﻿import { createContext, useState, useContext } from 'react';
+
+const InventoryContext = createContext();
+
+export const InventoryProvider = ({ children }) => {
+    const [inventory, setInventory] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    return (
+        <InventoryContext.Provider value={{
+            inventory, setInventory,
+            loading, setLoading,
+            error, setError
+        }}>
+            {children}
+        </InventoryContext.Provider>
+    );
+};
+
+export const useInventory = () => useContext(InventoryContext);
