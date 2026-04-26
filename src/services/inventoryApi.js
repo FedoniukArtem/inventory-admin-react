8cbede999e4c28a34fd4de6e4070c47ac7317924
Artem Fedoniuk web2025-1 <iiilssofff@gmail.com>
2026-04-26 20:55:36 +0300

    налаштування служби апі авторизації

--- src/services/inventoryApi.js
+++ src/services/inventoryApi.js
@@ -1,1 +1,27 @@
-﻿
+﻿import axios from 'axios';
+
+const API_BASE = 'http://localhost:5000/api';
+
+const api = axios.create({
+    baseURL: API_BASE,
+});
+
+export const inventoryApi = {
+    // Отримати список усіх позицій
+    getAll: () => api.get('/inventory'),
+
+    getById: (id) => api.get(`/inventory/${id}`),
+
+    // Створення (Multipart/form-data)
+    create: (formData) => api.post('/register', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+    }),
+
+    updateText: (id, data) => api.put(`/inventory/${id}`, data),
+
+    updatePhoto: (id, formData) => api.put(`/inventory/${id}/photo`, formData, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+    }),
+
+    delete: (id) => api.delete(`/inventory/${id}`),
+};
